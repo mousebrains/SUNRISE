cf2e9de09c6dd5e72e85d13463afdffaf885902f
Pat Welch <pat@mousebrains.com>
2021-06-20 16:01:14 +0000

    Fix crobjob

--- data_processing/cronjob.py
+++ data_processing/cronjob.py
@@ -2,6 +2,7 @@ import time
 import yaml
 from datetime import datetime, timezone, timedelta
 import os
+import subprocess
 
 # Job file template
 template_file = "/home/pat/SUNRISE/data_processing/cronjob_template.yml"
@@ -28,7 +29,7 @@ END_TIME = datetime.now().replace(tzinfo=timezone.utc)
 # for testing
 # END_TIME = datetime(2021,6,17,0,0,0,tzinfo=timezone.utc)
 
-START_TIME = END_TIME - timedelta(days=12)
+START_TIME = END_TIME - timedelta(days=2)
 
 # Make job file
 job_file = OUTPUT_DIR + "/crobjob-%s.yml" % datetime.now().strftime("%Y-%m-%dT%H-%M")
@@ -38,10 +39,21 @@ os.system("sed -e 's/#START_TIME#/%s/g' -e 's/#END_TIME#/%s/g' %s > %s" \
 
 # Plot
 try:
-    os.system('python3 %s %s' % (plot_file, job_file))
-    msg = "Done!\n"
+    # print time
+    msg = "Time window: %s -> %s\n" % (START_TIME.isoformat(), END_TIME.isoformat())
     print(msg)
     _ = clog_f.write(msg)
+
+    # make plots
+    cmd = ["/usr/bin/python3", plot_file, job_file]
+    process = subprocess.run(cmd, shell=False, check=False,
+                    stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+                    universal_newlines=True)
+    print(process.stdout)
+    print('Done!\n')
+    _ = clog_f.write(process.stdout)
+    _ = clog_f.write('Done!\n')
+    #os.system('python3 %s %s' % (plot_file, job_file))
 except:
     errmsg = "Error! Retry in 30 minutes\n"
     print(errmsg)
