56112a2ab8322ee402c4450d07dc0188a67ba91f
Pat Welch <pat@mousebrains.com>
2021-06-21 07:03:57 +0000

    correct WS FTMET file name

--- data_processing/realtime.py
+++ data_processing/realtime.py
@@ -9,11 +9,11 @@ import sunrise
 # DATAPATHS
 OUT_DIRECTORY ="/home/pat/Processed"
 PELICAN_FT_DATAPATHS = ["/home/pat/Dropbox/Pelican/MIDAS/MIDAS_001.elg"]
-WS_FT_DATAPATH = "/home/pat/Dropbox/WaltonSmith/test/WS21163_Hetland_TR-Full-Vdl.dat"
+WS_FT_DATAPATH = "/home/pat/Dropbox/WaltonSmith/FTMET/WS21163_Hetland-Full Vdl.dat"
 PELICAN_600_DATA = "/home/pat/Dropbox/Pelican/ADCP/wh600.nc"
 PELICAN_1200_DATA = "/home/pat/Dropbox/Pelican/ADCP/wh1200.nc"
-WS_600_DATA = "/home/pat/Dropbox/WaltonSmith/test/wh600.nc"
-WS_1200_DATA = "/home/pat/Dropbox/WaltonSmith/test/wh600.nc"
+WS_600_DATA = "/home/pat/Dropbox/WaltonSmith/ADCP/wh600.nc"
+WS_1200_DATA = "/home/pat/Dropbox/WaltonSmith/ADCP/wh600.nc"
 
 
 parser = argparse.ArgumentParser()
