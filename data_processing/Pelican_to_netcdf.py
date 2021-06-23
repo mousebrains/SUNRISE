9e5e2bbe1b3227c1bede0c164ac6b16e3f114e88
Jamie-Hilditch <42470546+JamieHilditch@users.noreply.github.com>
2021-06-23 17:15:35 -0500

    more debugging

--- data_processing/Pelican_to_netcdf.py
+++ data_processing/Pelican_to_netcdf.py
@@ -36,7 +36,7 @@ try:
     else:
         last_time = rootgrp["time"][-1]
         print(last_time)
-    start_time = datetime.datetime(year=2019,month=1,day=1,tzinfo=datetime.timezone.utc) + datetime.timedelta(seconds=last_time)
+    start_time = datetime.datetime(year=2019,month=1,day=1,tzinfo=datetime.timezone.utc) + datetime.timedelta(seconds=last_time[:])
 
 
     for filename in files:
