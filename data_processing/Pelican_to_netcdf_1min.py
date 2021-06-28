259bac7cf435548f143196f9becbee35dead420c
Jamie-Hilditch <42470546+JamieHilditch@users.noreply.github.com>
2021-06-28 01:43:54 -0500

    missing /

--- data_processing/Pelican_to_netcdf_1min.py
+++ data_processing/Pelican_to_netcdf_1min.py
@@ -5,7 +5,7 @@ import os
 import datetime
 
 NETCDF_FILE = r"/mnt/NAS/PE21_24_Shearman/data/met/2021 SUNRISE data/Pelican_FTMET_1min.nc"
-MIDAS_DIRECTORY = "home/pat/Dropbox/Pelican/MIDAS"#"/mnt/GOM/DATALOG40/EventData/MIDAS/"
+MIDAS_DIRECTORY = "/home/pat/Dropbox/Pelican/MIDAS"#"/mnt/GOM/DATALOG40/EventData/MIDAS/"
 
 variables = ["Heading", "Heading2", "Depth", "Temperature", "Salinity", "Conductivity", "SoundVelocity",
     "Transmission", "Fluorescence", "SPAR-Voltage", "SPAR-Microeinsteins", "BaroPressure", "AirTemp",
