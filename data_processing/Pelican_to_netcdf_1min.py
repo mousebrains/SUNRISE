12f4f0eb161ce71e4cbbb3775ff16b40bfabe4f0
Jamie-Hilditch <42470546+JamieHilditch@users.noreply.github.com>
2021-06-28 01:59:13 -0500

    print(time_seconds)

--- data_processing/Pelican_to_netcdf_1min.py
+++ data_processing/Pelican_to_netcdf_1min.py
@@ -72,7 +72,8 @@ try:
                 time = datetime.time.fromisoformat(time)
                 time = datetime.datetime.combine(date, time, tzinfo=datetime.timezone.utc)
                 time_seconds = (time - datetime.datetime(year=2021,month=1,day=1,tzinfo=datetime.timezone.utc)).total_seconds()
-                if time_seconds//60 > current_minute and time_seconds > last_minute:
+                if (time_seconds//60 > current_minute) and (time_seconds > last_minute):
+                    print(time_seconds)
                     for var in minute_data:
                         data[var].append(np.nanmean(minute_data[var]))
                         minute_data[var] = []
