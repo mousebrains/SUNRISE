744bbe4e4fe960f32a999cd91dc5e5844f0b2c6c
Jamie-Hilditch <42470546+JamieHilditch@users.noreply.github.com>
2021-06-28 00:43:28 -0500

    pelican 1 minute file

---
+++ data_processing/Pelican_to_netcdf_1min.py
@@ -0,0 +1,81 @@
+import netCDF4
+import numpy as np
+import csv
+import os
+import datetime
+
+NETCDF_FILE = r"/mnt/NAS/PE21_24_Shearman/data/met/2021 SUNRISE data"
+MIDAS_DIRECTORY = "/mnt/GOM/DATALOG40/EventData/MIDAS/"
+
+variables = ["Heading", "Depth", "Temperature", "Salinity", "Conductivity", "AirTemp", "BaroPressure",
+    "RelHumidity", "WindDirection", "WindSpeed"]
+midas = {"Heading": "Sperry-MK1-Gyro-Hdg-deg",
+    "Depth": "Knudsen-True-Depth-DRV-VALUE",
+    "Temperature": "Thermosalinograph-Data-Temp",
+    "Salinity": "Thermosalinograph-Data-Salinity",
+    "Conductivity": "Thermosalinograph-Data-Conductivity",
+    "AirTemp": "Air-Temp-1",
+    "BaroPressure": "BaromPress-1",
+    "RelHumidity": "Rel-Humidity-1",
+    "WindDirection": "TrueWindDirection-1-DRV-DIRECTION",
+    "WindSpeed": "TrueWindDirection-1-DRV-SPEED"}
+data = {"time": [],
+    "Lon": [],
+    "Lat": []}
+for var in variables:
+    data[var] = []
+files = os.listdir(MIDAS_DIRECTORY)
+files = [file for file in files if file[:6] == "MIDAS_" and file[-4:] == ".elg"]
+files = sorted(files)
+
+try:
+    rootgrp = netCDF4.Dataset(NETCDF_FILE, "r+", format="NETCDF4")
+    rootgrp.set_auto_mask(False)
+    skip_old = rootgrp.dimensions["time"].size
+    if skip_old == 0:
+        last_minute = 0
+    else:
+        last_minute = rootgrp["time"][-1]
+    # start_time = datetime.datetime(year=2019,month=1,day=1,tzinfo=datetime.timezone.utc) + datetime.timedelta(seconds=last_time)
+    current_minute = 0
+    minute_data = {}
+    for var in variables:
+        minute_data[var] = []
+
+    for filename in files:
+        with open(os.path.join(MIDAS_DIRECTORY,filename), newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                time = row["Time"]
+                date = row["Date"]
+                date = datetime.datetime.strptime(date,"%m/%d/%Y")
+                time = datetime.time.fromisoformat(time)
+                time = datetime.datetime.combine(date, time, tzinfo=datetime.timezone.utc)
+                time_seconds = (time - datetime.datetime(year=2021,month=1,day=1,tzinfo=datetime.timezone.utc)).total_seconds()
+                if time_seconds//60 > current_minute and time_seconds > last_time:
+                    for var in minute_data:
+                        data[var].append(np.nanmean(minute_data[var]))
+                        minute_data[var] = []
+                    current_minute = time_seconds//60
+                if time_seconds > last_time:
+                    lat = row["ADU800-GGA-Lat"]
+                    minute_data["Lat"].append(float(lat[0:2]) + float(lat[2:-1])/60)
+                    lon = row["ADU800-GGA-Lon"]
+                    minute_data["Lon"].append(-1*float(lon[0:3]) - float(lon[3:-1])/60)
+                    minute_data["time"].append((time_seconds//60)*60)
+                    for var in variables:
+                        value = row[midas[var]]
+                        if not value:
+                            value = "nan"
+                        try:
+                            minute_data[var].append(float(value))
+                        except:
+                            print("Error converting " + var + "to float")
+                            minute_data[var].append(float("nan"))
+    for key in minute_data:
+        minute_data[key] = np.array(minute_data[key])
+    tidx = minute_data["time"].size
+    for key in minute_data:
+        rootgrp[key][skip_old:skip_old+tidx] = minute_data[key]
+except:
+    raise
