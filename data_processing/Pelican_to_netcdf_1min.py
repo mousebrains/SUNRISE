93d6384c9e2340bee3e5705046f58f8b704d8918
Jamie-Hilditch <42470546+JamieHilditch@users.noreply.github.com>
2021-06-28 01:47:52 -0500

    added time, lat and lon to minute_data

--- data_processing/Pelican_to_netcdf_1min.py
+++ data_processing/Pelican_to_netcdf_1min.py
@@ -56,7 +56,9 @@ try:
         last_minute = rootgrp["time"][-1]
     # start_time = datetime.datetime(year=2019,month=1,day=1,tzinfo=datetime.timezone.utc) + datetime.timedelta(seconds=last_time)
     current_minute = 0
-    minute_data = {}
+    minute_data = {"time": [],
+        "Lon": [],
+        "Lat": []}
     for var in variables:
         minute_data[var] = []
 
